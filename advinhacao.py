48942c582eccee2e0b3d300be18c3042a3dee55e
Pietro Souza <pietro.g.souza@aluno.senai.br>
2025-02-27 17:28:12 -0300

    ADD: jogo finalizado e testado

--- advinhacao.py
+++ advinhacao.py
@@ -1,2 +1,11 @@
 #JOGO DE ADIVINHAÇÃO
 #DESENVOLVIDO POR PIETRO SOUZA
+import random
+#Gerando um numero aleatório
+numero_aleatorio = random.randint(1,10)
+
+pergunta = int(input("chuta um numero de 1 a 10 "))
+if pergunta == numero_aleatorio:
+    print (f"parabens voçe acertou o numero é {numero_aleatorio}")
+else:
+    print ("voçe errou melhorar")
