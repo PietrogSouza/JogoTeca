dcae11faf5196b4378da336ae6389e4eb9316859
Pietro <pietro.g.souza@aluno.senai.br>
2025-03-07 09:33:07 -0300

    Criei Jogo do impar ou par

--- ImparPar.py
+++ ImparPar.py
@@ -1,5 +1,15 @@
 import random
+imparpar = input("impar ou par: ")
+
 numero1 = random.randint(1,10)
 numero2 = int(input("me diga um numero "))
+
+print(f"o meu numero é: {numero1}")
+
 resultado1 = numero1 + numero2
 resultado2 = resultado1 % 2
+
+if (imparpar == "impar" and resultado2 == 1) or (imparpar == "par" and resultado2 == 0):
+    print("Parabens, você ganhou")
+else:
+    print("Você Perdeu")
